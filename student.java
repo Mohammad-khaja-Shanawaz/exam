7401e2e162718a44233c89a8a0882eb67d8fffef
Mohammad-khaja-Shanawaz <shanawaz.201550@gmail.com>
2025-07-26 10:49:48 +0530

    Add files via upload

---
+++ student.java
@@ -0,0 +1,13 @@
+public class StudentDetails { 
+    public static void main(String[] args) { 
+        String name = "sai ram"; 
+        String rollNo = "22CS001"; 
+        String department = "Computer Science"; 
+ 
+        System.out.println("Student Details:"); 
+        System.out.println("Name      : " + name); 
+System.out.println("Roll No   : " + rollNo); 
+System.out.println("Department: " + department); 
+} 
+} 
+
